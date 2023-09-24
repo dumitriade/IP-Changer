198478bc0eb0e0973e8076fe4d68154072438b4a
Ovi <Ovi@PC-OVI>
2023-09-23 21:15:19 -0400

    Add project files.

---
+++ IP Changer/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace IP_Changer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ipTextbox = new System.Windows.Forms.TextBox();
+            this.subnetTextbox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.gatewayTextbox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.goButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripSeparator1});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 10);
+            this.contextMenuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.contextMenuStrip_ItemClicked);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(57, 6);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(10, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 26);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "IP address";
+            // 
+            // ipTextbox
+            // 
+            this.ipTextbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ipTextbox.Location = new System.Drawing.Point(132, 5);
+            this.ipTextbox.Name = "ipTextbox";
+            this.ipTextbox.Size = new System.Drawing.Size(191, 32);
+            this.ipTextbox.TabIndex = 4;
+            this.ipTextbox.Text = "192.168.0.99";
+            // 
+            // subnetTextbox
+            // 
+            this.subnetTextbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.subnetTextbox.Location = new System.Drawing.Point(132, 43);
+            this.subnetTextbox.Name = "subnetTextbox";
+            this.subnetTextbox.Size = new System.Drawing.Size(191, 32);
+            this.subnetTextbox.TabIndex = 6;
+            this.subnetTextbox.Text = "255.255.255.0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(49, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 26);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "subnet";
+            // 
+            // gatewayTextbox
+            // 
+            this.gatewayTextbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gatewayTextbox.Location = new System.Drawing.Point(132, 81);
+            this.gatewayTextbox.Name = "gatewayTextbox";
+            this.gatewayTextbox.Size = new System.Drawing.Size(191, 32);
+            this.gatewayTextbox.TabIndex = 8;
+            this.gatewayTextbox.Text = "192.168.0.1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(33, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 26);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "gateway";
+            // 
+            // goButton
+            // 
+            this.goButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.goButton.Location = new System.Drawing.Point(15, 130);
+            this.goButton.Name = "goButton";
+            this.goButton.Size = new System.Drawing.Size(308, 41);
+            this.goButton.TabIndex = 9;
+            this.goButton.Text = "Go";
+            this.goButton.UseVisualStyleBackColor = true;
+            this.goButton.Click += new System.EventHandler(this.goButton_Click);
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cancelButton.Location = new System.Drawing.Point(15, 177);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(146, 41);
+            this.cancelButton.TabIndex = 10;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exitButton.Location = new System.Drawing.Point(178, 177);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(145, 41);
+            this.exitButton.TabIndex = 11;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(335, 230);
+            this.ControlBox = false;
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.goButton);
+            this.Controls.Add(this.gatewayTextbox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.subnetTextbox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ipTextbox);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "IP Changer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox ipTextbox;
+        private System.Windows.Forms.TextBox subnetTextbox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox gatewayTextbox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button goButton;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ IP Changer/Form1.cs
@@ -0,0 +1,308 @@
+﻿using System;
+using System.Management;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+using System.Net.NetworkInformation;
+
+namespace IP_Changer
+{
+    public partial class Form1 : Form
+    {
+
+        public historyObj[] historyObjs = { };
+        public nic[] nics = { };
+        public string targetNic = "";
+        int maxHistory = 5;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            Rectangle workingArea = Screen.GetWorkingArea(this);
+            this.Location = new Point(workingArea.Right - Size.Width - 50,
+                                      workingArea.Bottom - Size.Height - 50);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            notifyIcon1.BalloonTipIcon = ToolTipIcon.Info;
+            notifyIcon1.BalloonTipText = "I am a NotifyIcon Balloon";
+            notifyIcon1.BalloonTipTitle = "Welcome Message";
+            //            notifyIcon1.ShowBalloonTip(1000);
+
+            this.Hide();
+            goButton.Enabled = false;
+
+            updateNics();
+            updateMenu();
+        }
+
+        private void updateNics()
+        {
+            List<NetworkInterface> values = new List<NetworkInterface>();
+            foreach (NetworkInterface netInt in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                nic n = new nic();
+                n.name = netInt.Name;
+                n.description = netInt.Description;
+
+                nics = nics.Concat(new nic[] { n }).ToArray();
+
+            }
+        }
+        private void updateMenu()
+        {
+            contextMenuStrip1.Items.Clear();
+            contextMenuStrip1.Items.Add("DHCP");
+            contextMenuStrip1.Items.Add("Custom");
+
+            contextMenuStrip1.Items.Add("-");
+
+            if (historyObjs.Length > 0)
+            {
+                for (int i = 0; i < historyObjs.Length; i++)
+                {
+                    historyObj h = historyObjs[i];
+
+                    ToolStripMenuItem newMenuItem = new ToolStripMenuItem();
+
+                    newMenuItem.Text = h.toString;
+                    newMenuItem.Tag = i;
+
+                    contextMenuStrip1.Items.Add(newMenuItem);
+
+                }
+                contextMenuStrip1.Items.Add("-");
+            }
+
+            ToolStripMenuItem item = new ToolStripMenuItem();
+            item.Text = "Target NIC";
+            item.Tag = -1;
+
+            for(int i = 0; i < nics.Length; i++)
+            {
+                nic n = nics[i];
+                item.DropDownItems.Add(n.name);
+
+                item.DropDownItems[i].Tag = i;
+
+            }
+            item.DropDownItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.nicSelection_ItemClicked);
+
+            contextMenuStrip1.Items.Add(item);
+            contextMenuStrip1.Items.Add("Exit");
+
+        }
+
+        private void goButton_Click(object sender, EventArgs e)
+        {
+            historyObj his = new historyObj();
+            his.ip = ipTextbox.Text;
+            his.gateway = gatewayTextbox.Text;
+            his.subnet = subnetTextbox.Text;
+
+            his.toString = ipTextbox.Text + "/24"; //TODO: properly handle subnet 0 128 192 224 240 248 252 254 255
+
+            if (historyObjs.Length > maxHistory)
+                historyObjs = historyObjs.Skip(1).ToArray();
+
+            historyObjs = historyObjs.Concat(new historyObj[] { his }).ToArray();
+
+            this.Hide();
+            updateMenu();
+            NetworkConfigurator.net_adapters();
+        }
+
+        private void contextMenuStrip_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            Console.WriteLine(e.ClickedItem.Text);
+
+            if (e.ClickedItem.Text == "DHCP")
+            {
+                Console.WriteLine("will change to DHCP");
+                _ = NetworkConfigurator.SetDHCP(targetNic);
+            }
+            else if (e.ClickedItem.Text == "Custom")
+            {
+                this.Show();
+            }
+            else if(e.ClickedItem.Text == "Target NIC")
+            {
+
+            }
+            else
+            {
+                historyObj h = historyObjs[(int)e.ClickedItem.Tag];
+
+                Console.WriteLine("Will change to " + h.ip + " / " + h.subnet + " / " + h.gateway);
+
+                NetworkConfigurator.SetIP(targetNic, h.ip, h.subnet, h.gateway);
+            }
+        }
+
+        private void nicSelection_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            Console.WriteLine("clicked item " + (int)e.ClickedItem.Tag);
+            nic n = nics[(int)e.ClickedItem.Tag];
+            Console.WriteLine("Target nic is " + n.name + ": " + n.description);
+
+            targetNic = n.description;
+
+            goButton.Enabled = true;
+        }
+
+        private void closeButton_Click(object sender, FormClosingEventArgs e)
+        {
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                e.Cancel = true;
+                Hide();
+            }
+        }
+
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+
+    public class historyObj {
+        public string ip { set; get; }
+        public string subnet { set; get; }
+        public string gateway { set; get; }
+
+        public string toString { set; get; }
+    }
+
+    public class nic
+    {
+        public string name { set; get; }
+        public string description { set; get; }
+    }
+
+    public static class NetworkConfigurator
+    {
+        public static System.Collections.Generic.List<NetworkInterface> net_adapters()
+        {
+            List<NetworkInterface> values = new List<NetworkInterface>();
+            foreach (NetworkInterface nic in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                values.Add(nic);
+            }
+            return values;
+        }
+
+        public static bool SetDHCP(string nicName)
+        {
+            ManagementClass mc = new ManagementClass("Win32_NetworkAdapterConfiguration");
+            ManagementObjectCollection moc = mc.GetInstances();
+
+            NetworkInterface[] interfaces = NetworkInterface.GetAllNetworkInterfaces();
+            NetworkInterface networkInterface = interfaces.FirstOrDefault(x => x.Name == nicName);
+            string nicDesc = nicName;
+
+            if (networkInterface != null)
+            {
+                nicDesc = networkInterface.Description;
+            }
+
+            foreach (ManagementObject mo in moc)
+            {
+                if ((bool)mo["IPEnabled"] == true
+                    && mo["Description"].Equals(nicDesc) == true)
+                {
+                    try
+                    {
+                        ManagementBaseObject newDNS = mo.GetMethodParameters("SetDNSServerSearchOrder");
+
+                        newDNS["DNSServerSearchOrder"] = null;
+                        ManagementBaseObject enableDHCP = mo.InvokeMethod("EnableDHCP", null, null);
+                        ManagementBaseObject setDNS = mo.InvokeMethod("SetDNSServerSearchOrder", newDNS, null);
+                    }
+                    catch
+                    {
+                        return false;
+                    }
+                }
+            }
+            return false;
+
+        }
+
+            public static void SetIP(string description, string ip, string subnet, string gateway)
+        {
+            string subnetMask = subnet;
+            string address = ip;
+
+            var adapterConfig = new ManagementClass("Win32_NetworkAdapterConfiguration");
+            var networkCollection = adapterConfig.GetInstances();
+
+            foreach (ManagementObject adapter in networkCollection)
+            {
+                string d = adapter["Description"] as string;
+                if (string.Compare(d, description, StringComparison.InvariantCultureIgnoreCase) == 0)
+                {
+                    try
+                    {
+                        // Set DefaultGateway
+                        var newGateway = adapter.GetMethodParameters("SetGateways");
+                        newGateway["DefaultIPGateway"] = new string[] { gateway };
+                        newGateway["GatewayCostMetric"] = new int[] { 1 };
+
+                        // Set IPAddress and Subnet Mask
+                        var newAddress = adapter.GetMethodParameters("EnableStatic");
+                        newAddress["IPAddress"] = new string[] { address };
+                        newAddress["SubnetMask"] = new string[] { subnetMask };
+
+                        adapter.InvokeMethod("EnableStatic", newAddress, null);
+                        adapter.InvokeMethod("SetGateways", newGateway, null);
+
+                        Console.WriteLine("Updated to static IP address!");
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Unable to Set IP : " + ex.Message);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Set's the DNS Server of the local machine
+        /// </summary>
+        /// <param name="nic">NIC address</param>
+        /// <param name="dnsServers">Comma seperated list of DNS server addresses</param>
+        /// <remarks>Requires a reference to the System.Management namespace</remarks>
+        public static void SetNameservers(string nicDescription, string[] dnsServers)
+        {
+            using (var networkConfigMng = new ManagementClass("Win32_NetworkAdapterConfiguration"))
+            {
+                using (var networkConfigs = networkConfigMng.GetInstances())
+                {
+                    foreach (var managementObject in networkConfigs.Cast<ManagementObject>().Where(mo => (bool)mo["IPEnabled"] && (string)mo["Description"] == nicDescription))
+                    {
+                        using (var newDNS = managementObject.GetMethodParameters("SetDNSServerSearchOrder"))
+                        {
+                            newDNS["DNSServerSearchOrder"] = dnsServers;
+                            managementObject.InvokeMethod("SetDNSServerSearchOrder", newDNS, null);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+
